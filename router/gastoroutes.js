3f0f024cb29d904060d4c52509fbfb5b10cee4bf
andrese7 <ca.echeverry20@gmail.com>
2020-04-16 20:07:46 -0500

    '1' of null

--- router/gastoroutes.js
+++ router/gastoroutes.js
@@ -20,19 +20,19 @@ router.post('/gastos', (req, res) => {
       .catch(err => res.status(400).json(err));
   });
   
-  router.get('/api/v1/gastos/:id', (req, res) => {
+  router.get('/gastos/:id', (req, res) => {
     Gasto.findById(req.params.id)
       .then(gasto => res.status(200).json(gasto))
       .catch(err => res.status(404).json(err));
   });
   
-  router.patch('/api/v1/gastos/:id', (req, res) => {
+  router.patch('/gastos/:id', (req, res) => {
     Gasto.findByIdAndUpdate(req.params.id, req.body, { new: true })
       .then(gasto => res.status(200).json(gasto))
       .catch(err => res.status(404).json(err));
   });
   
-  router.delete('/api/v1/gastos/:id', (req, res) => {
+  router.delete('/gastos/:id', (req, res) => {
     Gasto.findByIdAndDelete(req.params.id)
       .then(() => res.status(204).json())
       .catch(err => res.status(404).json(err));
